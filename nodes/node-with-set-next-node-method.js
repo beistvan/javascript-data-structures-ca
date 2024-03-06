1e62d88446e53d432eabee536b8de7a5a8fd4b29
Beregszászi István <beistvan@gmail.com>
2024-03-06 22:06:00 +0100

    Node With Set Next Node Method

---
+++ nodes/node-with-set-next-node-method.js
@@ -0,0 +1,19 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+
+  setNextNode(node) {
+    this.next = node;
+  }
+}
+
+const firstNode = new Node('I am an instance of a Node!');
+const secondNode = new Node('I am a second Node!');
+
+firstNode.setNextNode(secondNode);
+console.log(firstNode);
+
+module.exports = Node;
+
